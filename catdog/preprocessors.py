2e60dfc01e9afa4e76cea093176e25c13d88873d
dmrib <dmrib.cs@gmail.com>
2017-07-05 23:13:15 -0300

    Find default image size for dataset

--- catdog/preprocessors.py
+++ catdog/preprocessors.py
@@ -18,7 +18,7 @@ class Dataset():
 
         """
         self.data = self.load_images(paths)
-        self.default_width, self.default_height = self.compute_defaults()
+        self.default_width, self.default_height = self.compute_default_size()
 
     def load_images(self, paths):
         """Read every image in paths list.
@@ -35,6 +35,29 @@ class Dataset():
             sample = Sample(image_path)
             images.append(sample)
 
+        return images
+
+    def compute_default_size(self):
+        """Compute the maximum image width and height in the dataset.
+
+        Args:
+            paths (list): paths of images to be labeled.
+        Returns:
+            width (int): maximum image width.
+            height (int): maximum image height.
+
+        """
+        height = 1000000
+        width = 1000000
+        for image in self.data:
+            w, h = image.shape
+            if w < width:
+                width = w
+            if h < height:
+                height = h
+
+        return width, height
+
 
 class Sample():
     """Abstraction for a dataset image sample."""
@@ -49,6 +72,7 @@ class Sample():
 
         """
         self.image = cv2.imread(path)
+        self.shape = self.image.shape[:2]
 
     def show(self):
         """Display image.
@@ -77,4 +101,4 @@ class Sample():
 
 
 if __name__ == '__main__':
-    s = Dataset(dataset.get_images_paths())
+    s = Dataset(dataset.get_images_paths()[:100])
