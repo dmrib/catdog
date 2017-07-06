34580310eef06d76a9cbbdbad426617f615d2b8e
dmrib <dmrib.cs@gmail.com>
2017-07-05 22:25:12 -0300

    Read and display images

--- catdog/preprocessors.py
+++ catdog/preprocessors.py
@@ -1,6 +1,37 @@
-"""Image preprocessing functionality.
+"""Image preprocessing."""
 
-This module contains the implementation of image preprocessing functions
-required for the dataset further analysis.
 
-"""
+import cv2
+
+
+class Sample():
+    """Abstraction for a dataset image sample."""
+
+    def __init__(self, path):
+        """Initializer.
+
+        Args:
+            path(str): sample image path.
+        Returns:
+            None.
+
+        """
+        self.image = cv2.imread(path)
+
+    def show(self):
+        """Display image.
+
+        Args:
+            None.
+        Returns:
+            None.
+            
+        """
+        cv2.imshow('Image', self.image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    s = Sample('../data/cat.0.jpg')
+    s.show()
