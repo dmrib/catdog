7cfd33203a43afd3c98ee657ed71452db55fce79
dmrib <dmrib.cs@gmail.com>
2017-07-05 22:31:34 -0300

    Resize image

--- catdog/preprocessors.py
+++ catdog/preprocessors.py
@@ -25,13 +25,27 @@ class Sample():
             None.
         Returns:
             None.
-            
+
         """
         cv2.imshow('Image', self.image)
         cv2.waitKey()
         cv2.destroyAllWindows()
 
+    def resize(self, width, height):
+        """Resize image for given width and height.
+
+        Args:
+            None.
+        Returns:
+            None.
+
+        """
+        self.image = cv2.resize(self.image, (width, height),
+                                interpolation=cv2.INTER_AREA)
+
 
 if __name__ == '__main__':
     s = Sample('../data/cat.0.jpg')
+    s.show()
+    s.resize(500, 500)
     s.show()
