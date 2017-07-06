5372eb25db15ec9a7510fa17f52d5244bba971a4
dmrib <dmrib.cs@gmail.com>
2017-07-05 21:16:43 -0300

    Read images paths

--- catdog/dataset.py
+++ catdog/dataset.py
@@ -3,3 +3,18 @@
 This module deals with operation within the dataset concerning the preparation
 for experiments.
 """
+
+import glob
+
+
+def get_images_names(pattern='../data/*.jpg'):
+    """Return image paths that matches glob pattern.
+
+    Args:
+        pattern (str): glob pattern.
+    Returns:
+        paths (list): contains all image paths.
+
+    """
+    paths = glob.glob(pattern)
+    return paths
