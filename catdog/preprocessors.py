b392c30d61cd882afd74db58fdb9c052b63f8afc
dmrib <dmrib.cs@gmail.com>
2017-07-06 00:16:31 -0300

    Resize entire dataset

--- catdog/preprocessors.py
+++ catdog/preprocessors.py
@@ -2,6 +2,7 @@
 
 
 import cv2
+import numpy as np
 import dataset
 
 
@@ -18,7 +19,8 @@ class Dataset():
 
         """
         self.data = self.load_images(paths)
-        self.default_width, self.default_height = self.compute_default_size()
+        self.dimensions = self.compute_default_size(use_mean=True)
+        self.resize(self.dimensions)
 
     def load_images(self, paths):
         """Read every image in paths list.
@@ -37,8 +39,8 @@ class Dataset():
 
         return images
 
-    def compute_default_size(self):
-        """Compute the maximum image width and height in the dataset.
+    def compute_default_size(self, use_mean=False):
+        """Compute the maximum and mean image width and height in the dataset.
 
         Args:
             paths (list): paths of images to be labeled.
@@ -47,16 +49,33 @@ class Dataset():
             height (int): maximum image height.
 
         """
-        min_height = 1000000
-        min_width = 1000000
+        widths = []
+        heights = []
         for image in self.data:
             width, height = image.shape
-            if width < min_width:
-                min_width = width
-            if height < min_height:
-                height = min_height
+            widths.append(width)
+            heights.append(height)
 
-        return min_width, min_height
+        if use_mean:
+            return (int(np.mean(widths)), int(np.mean(heights)))
+        else:
+            return (min(widths), min(heights))
+
+    def resize(self, dimensions):
+        """Resize entire dataset to default width and height.
+
+        Args:
+            dimensions (tuple): dimensions of the resized dataset.
+        Returns:
+            None.
+
+        """
+        for image in self.data:
+            image.resize(dimensions[0], dimensions[1])
+
+        self.data[0].show()
+        self.data[-1].show()
+        self.data[5].show()
 
 
 class Sample():
@@ -101,4 +120,4 @@ class Sample():
 
 
 if __name__ == '__main__':
-    s = Dataset(dataset.get_images_paths()[:100])
+    s = Dataset(dataset.get_images_paths()[:10])
