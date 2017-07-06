36d04abf53967bb3603814efd9b67969a6c52060
dmrib <dmrib.cs@gmail.com>
2017-07-05 21:58:35 -0300

    Separate training and validation set

--- catdog/dataset.py
+++ catdog/dataset.py
@@ -6,6 +6,7 @@ for experiments.
 
 import glob
 import os
+import random
 
 
 def get_images_names(pattern='../data/*.jpg'):
@@ -36,3 +37,21 @@ def label_images(paths):
         image_labels[path] = os.path.basename(path)[:3]
 
     return image_labels
+
+
+def training_and_validation_set(paths):
+    """Separates dataset in training and test set.
+
+    Args:
+        paths (list): paths of images to be labeled.
+    Returns:
+        training_set (list): list of paths of the training set images.
+        validation_set (list): list of paths of the validation set images.
+
+    """
+    paths = paths[:]
+    random.shuffle(paths)
+    training_set = paths[:round(len(paths)/10)]
+    validation_set = paths[round(len(paths)/10):]
+
+    return training_set, validation_set
