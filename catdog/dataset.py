665f30bb4d639539a4ab209695419f0470928265
dmrib <dmrib.cs@gmail.com>
2017-07-05 21:29:35 -0300

    Pair image path and label

--- catdog/dataset.py
+++ catdog/dataset.py
@@ -5,6 +5,7 @@ for experiments.
 """
 
 import glob
+import os
 
 
 def get_images_names(pattern='../data/*.jpg'):
@@ -18,3 +19,20 @@ def get_images_names(pattern='../data/*.jpg'):
     """
     paths = glob.glob(pattern)
     return paths
+
+
+def label_images(paths):
+    """Create dictionary pairing image path and her label.
+
+    Args:
+        paths (list): paths of images to be labeled.
+    Returns:
+        image_labels (dict): dictionary with image path as key and label as
+                             value.
+
+    """
+    image_labels = {}
+    for path in paths:
+        image_labels[path] = os.path.basename(path)[:3]
+
+    return image_labels
