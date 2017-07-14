d481deddaa9c42ee364cfebe738bd8dfd6d844f4
dmrib <dmrib.cs@gmail.com>
2017-07-13 23:25:00 -0300

    K neighbors classifier experiment

--- catdog/experiments.py
+++ catdog/experiments.py
@@ -184,3 +184,60 @@ def random_forest_classifier(filters, n_estimators, criterion, verbose=False):
     print('\nMean score: ', mean_score)
     print('Standard deviation: ', std_deviation)
     print('F: ', f)
+
+
+def k_nearest(filters, n_neighbors, weights, verbose=False):
+    """K nearest neighbors classifier experiment."""
+    # Dataset folder operations
+    if verbose:
+        print('Getting dataset images paths...')
+    paths = pps.get_images_paths('../data/training/*.jpg')
+
+    # Loading dataset
+    if verbose:
+        print('Loading images dataset...')
+    input_data = dataset.Dataset(paths, filters=filters, use_mean=True)
+
+    # Generating sintetic images
+    if verbose:
+        print('Generating sintetic dataset...')
+    input_data.generate_sintetic_dataset()
+
+    # Extract labels
+    if verbose:
+        print('Extracting labels...')
+    labels = input_data.labels_array()
+
+    # Input data matrix
+    if verbose:
+        print('Generating data matrix...')
+    data_matrix = input_data.compute_data_matrix()
+
+    # Compute PCA
+    if verbose:
+        print('Making principal component analysis...')
+    pca = PCA(n_components=25)
+    pca.fit_transform(data_matrix, y=labels)
+
+    # Instantiate random forest classifier model
+    if verbose:
+        print('Instantiating model...')
+    classifier = KNeighborsClassifier(n_jobs=-1, n_neighbors=n_neighbors,
+                                      weights=weights)
+
+    # Training random forest classifier model and computing score
+    if verbose:
+        print('Training and computing score...')
+    shuffle_split = ShuffleSplit(n_splits=15, test_size=0.15, train_size=0.85,
+                                 random_state=0)
+    scores = cross_val_score(estimator=classifier, X=data_matrix,
+                             y=labels, cv=shuffle_split,
+                             n_jobs=-1)
+
+    # Reporting results
+    mean_score = np.mean(scores)
+    std_deviation = np.std(scores)
+    f = mean_score - std_deviation
+    print('\nMean score: ', mean_score)
+    print('Standard deviation: ', std_deviation)
+    print('F: ', f)
