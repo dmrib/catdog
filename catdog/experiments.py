5d426f41c69f1f802e173f9ed5a0c176fae562ed
dmrib <dmrib.cs@gmail.com>
2017-07-13 21:06:05 -0300

    Suport vector classifier experiment

--- catdog/experiments.py
+++ catdog/experiments.py
@@ -5,12 +5,13 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.model_selection import ShuffleSplit, cross_val_score
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 import preprocessors as pps
 import dataset
 
 
-def neural_nets(verbose=False):
-    """First experiment."""
+def neural_nets(filters, layers, verbose=False):
+    """Neural nets experiment."""
     # Dataset folder operations
     if verbose:
         print('Getting dataset images paths...')
@@ -19,7 +20,7 @@ def neural_nets(verbose=False):
     # Loading dataset
     if verbose:
         print('Loading images dataset...')
-    input_data = dataset.Dataset(paths, filters='', use_mean=True)
+    input_data = dataset.Dataset(paths, filters=filters, use_mean=True)
 
     # Generating sintetic images
     if verbose:
@@ -45,7 +46,7 @@ def neural_nets(verbose=False):
     # Instanciate neural net model
     if verbose:
         print('Instantiating model...')
-    classifier = MLPClassifier(random_state=3, hidden_layer_sizes=10)
+    classifier = MLPClassifier(random_state=3, hidden_layer_sizes=layers)
 
     # Training neural net model and computing score
     if verbose:
@@ -60,10 +61,66 @@ def neural_nets(verbose=False):
     mean_score = np.mean(scores)
     std_deviation = np.std(scores)
     f = mean_score - std_deviation
-    print('Mean score: ', mean_score)
+    print('\nMean score: ', mean_score)
     print('Standard deviation: ', std_deviation)
     print('F: ', f)
 
 
-if __name__ == '__main__':
-    neural_nets(verbose=True)
+def suport_vector(filters, kernel, verbose=False, degree=None):
+    """Suport vector classifier experiment."""
+    # Dataset folder operations
+    if verbose:
+        print('Getting dataset images paths...')
+    paths = pps.get_images_paths('../data/training/*.jpg')
+
+    # Loading dataset
+    if verbose:
+        print('Loading images dataset...')
+    input_data = dataset.Dataset(paths, filters=filters, use_mean=True)
+
+    # Generating sintetic images
+    if verbose:
+        print('Generating sintetic dataset...')
+    input_data.generate_sintetic_dataset()
+
+    # Extract labels
+    if verbose:
+        print('Extracting labels...')
+    labels = input_data.labels_array()
+
+    # Input data matrix
+    if verbose:
+        print('Generating data matrix...')
+    data_matrix = input_data.compute_data_matrix()
+
+    # Compute PCA
+    if verbose:
+        print('Making principal component analysis...')
+    pca = PCA(n_components=25)
+    pca.fit_transform(data_matrix, y=labels)
+
+    # Instanciate suport vector classifier model
+    if verbose:
+        print('Instantiating model...')
+    if kernel != 'poly':
+        classifier = SVC(random_state=3, max_iter=500, kernel=kernel)
+    else:
+        classifier = SVC(random_state=3, max_iter=500, kernel=kernel,
+                         degree=degree)
+
+    # Training suport vector classifier model and computing score
+    if verbose:
+        print('Training and computing score...')
+    shuffle_split = ShuffleSplit(n_splits=15, test_size=0.15, train_size=0.85,
+                                 random_state=0)
+    scores = cross_val_score(estimator=classifier, X=data_matrix,
+                             y=labels, cv=shuffle_split,
+                             n_jobs=-1)
+
+    # Reporting results
+    mean_score = np.mean(scores)
+    std_deviation = np.std(scores)
+    f = mean_score - std_deviation
+    print('\nMean score: ', mean_score)
+    print('Standard deviation: ', std_deviation)
+    print('F: ', f)
