53b92ffe0fe83720a62bba51a5abef48442e3792
dmrib <dmrib.cs@gmail.com>
2017-07-16 00:04:33 -0300

    Refactor preprocessors module

--- catdog/preprocessors.py
+++ catdog/preprocessors.py
@@ -1,12 +1,10 @@
 """Dataset files operations and helpers."""
 
 import glob
-import os
-import random
 
 
 def get_images_paths(pattern='../data/*.jpg'):
-    """Return image paths that matches glob pattern.
+    """Return path of images that matches glob pattern.
 
     Args:
         pattern (str): glob pattern.
@@ -16,38 +14,3 @@ def get_images_paths(pattern='../data/*.jpg'):
     """
     paths = glob.glob(pattern)
     return paths
-
-
-def label_images(paths):
-    """Create dictionary pairing image path and her label.
-
-    Args:
-        paths (list): paths of images to be labeled.
-    Returns:
-        image_labels (dict): dictionary with image path as key and label as
-                             value.
-
-    """
-    image_labels = {}
-    for path in paths:
-        image_labels[path] = os.path.basename(path)[:3]
-
-    return image_labels
-
-
-def training_and_validation_set(paths, percentile=10):
-    """Separates dataset in training and test set.
-
-    Args:
-        paths (list): paths of images to be labeled.
-    Returns:
-        training_set (list): list of paths of the training set images.
-        validation_set (list): list of paths of the validation set images.
-
-    """
-    paths = paths[:]
-    random.shuffle(paths)
-    training_set = paths[:round(len(paths)/percentile)]
-    validation_set = paths[round(len(paths)/percentile):]
-
-    return training_set, validation_set
